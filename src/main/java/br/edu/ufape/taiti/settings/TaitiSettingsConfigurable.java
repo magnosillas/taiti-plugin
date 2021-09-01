e8d4bc4b1557b08bb74949949424fb76e14fb9cd
Gabriel Viana <gabrielalmeidavn@gmail.com>
2021-09-01 11:23:50 -0300

    implementation of persistence of sensitive user data in settings

--- src/main/java/br/edu/ufape/taiti/settings/TaitiSettingsConfigurable.java
+++ src/main/java/br/edu/ufape/taiti/settings/TaitiSettingsConfigurable.java
@@ -1,11 +1,16 @@
 package br.edu.ufape.taiti.settings;
 
 import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
+import org.jsoup.internal.StringUtil;
 
 import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class TaitiSettingsConfigurable implements Configurable {
 
@@ -44,17 +49,19 @@ public class TaitiSettingsConfigurable implements Configurable {
     }
 
     @Override
-    public void apply() {
-        // TODO: tratar os dados
+    public void apply() throws ConfigurationException {
         TaitiSettingsState settings = TaitiSettingsState.getInstance(project);
+        validate();
         settings.githubURL = component.getGithubURLText();
         settings.pivotalURL = component.getPivotalURLText();
         settings.token = component.getPivotalToken();
+        settings.storeCredentials(project);
     }
 
     @Override
     public void reset() {
         TaitiSettingsState settings = TaitiSettingsState.getInstance(project);
+        settings.retrieveStoredCredentials(project);
         component.setGithubURLText(settings.githubURL);
         component.setPivotalURLText(settings.pivotalURL);
         component.setPivotalToken(settings.token);
@@ -64,4 +71,42 @@ public class TaitiSettingsConfigurable implements Configurable {
     public void disposeUIResources() {
         component = null;
     }
+
+    private void validate() throws ConfigurationException {
+        // check if the fields is empty
+        if (StringUtil.isBlank(component.getGithubURLText())) {
+            throw new ConfigurationException("The GitHub URL field is empty", "Cannot Save Settings");
+        }
+        if (StringUtil.isBlank(component.getPivotalURLText())) {
+            throw new ConfigurationException("The PivotalTracker URL field is empty", "Cannot Save Settings");
+        }
+        if (StringUtil.isBlank(component.getPivotalToken())) {
+            throw new ConfigurationException("The PivotalTracker token field is empty", "Cannot Save Settings");
+        }
+
+        // check if the input data is valid
+        try {
+            new URL(component.getGithubURLText()).toURI();
+            String regex = "https://([w]{3}\\.)?github\\.com/.[^/]+/.[^/]+";
+            if (!component.getGithubURLText().matches(regex)) {
+                throw new MalformedURLException();
+            }
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new ConfigurationException(
+                    "The GitHub URL is malformed, example of URL: https://github.com/{user}/{repository}",
+                    "Cannot Save Settings");
+        }
+        try {
+            new URL(component.getPivotalURLText()).toURI();
+            String regex = "https://[w]{3}\\.pivotaltracker\\.com/n/projects/\\d+";
+            if (!component.getPivotalURLText().matches(regex)) {
+                throw new MalformedURLException();
+            }
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new ConfigurationException(
+                    "The PivotalTracker URL is malformed, example of URL: https://www.pivotaltracker.com/n/projects/{project_id}",
+                    "Cannot Save Settings");
+        }
+    }
+
 }
--- src/main/java/br/edu/ufape/taiti/settings/TaitiSettingsState.java
+++ src/main/java/br/edu/ufape/taiti/settings/TaitiSettingsState.java
@@ -1,34 +1,87 @@
 package br.edu.ufape.taiti.settings;
 
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.State;
-import com.intellij.openapi.components.Storage;
+import com.intellij.credentialStore.CredentialAttributes;
+import com.intellij.credentialStore.CredentialAttributesKt;
+import com.intellij.credentialStore.Credentials;
+import com.intellij.ide.passwordSafe.PasswordSafe;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.xmlb.XmlSerializerUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.project.ProjectUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 
-@State(
-        name = "br.edu.ufape.taiti.settings.TaitiSettingsState",
-        storages = {@Storage("TaitiSettingsPlugin.xml")}
-)
-public class TaitiSettingsState  implements PersistentStateComponent<TaitiSettingsState> {
-    public String githubURL = "";
-    public String pivotalURL = "";
-    public String token = "";
+public class TaitiSettingsState {
+    protected String githubURL = "";
+    protected String pivotalURL = "";
+    protected String token = "";
+
+    public String getGithubURL() {
+        return githubURL;
+    }
+
+    public String getPivotalURL() {
+        return pivotalURL;
+    }
+
+    public String getToken() {
+        return token;
+    }
 
     public static TaitiSettingsState getInstance(Project project) {
         return project.getService(TaitiSettingsState.class);
     }
 
-    @Nullable
-    @Override
-    public TaitiSettingsState getState() {
-        return this;
+    public void retrieveStoredCredentials(Project project) {
+        String keyGithubURL = "githubURL" + getProjectName(project);
+        String keyPivotalURL = "pivotalURL" + getProjectName(project);
+        String keyPivotalToken = "pivotalToken" + getProjectName(project);
+
+        CredentialAttributes credentialAttributes = createCredentialAttributes(keyGithubURL);
+        Credentials credentials = PasswordSafe.getInstance().get(credentialAttributes);
+        if (credentials != null) {
+            this.githubURL = credentials.getPasswordAsString();
+        }
+
+        credentialAttributes = createCredentialAttributes(keyPivotalURL);
+        credentials = PasswordSafe.getInstance().get(credentialAttributes);
+        if (credentials != null) {
+            this.pivotalURL = credentials.getPasswordAsString();
+        }
+
+        credentialAttributes = createCredentialAttributes(keyPivotalToken);
+        credentials = PasswordSafe.getInstance().get(credentialAttributes);
+        if (credentials != null) {
+            this.token = credentials.getPasswordAsString();
+        }
     }
 
-    @Override
-    public void loadState(@NotNull TaitiSettingsState state) {
-        XmlSerializerUtil.copyBean(state, this);
+    public void storeCredentials(Project project) {
+        String keyGithubURL = "githubURL" + getProjectName(project);
+        String keyPivotalURL = "pivotalURL" + getProjectName(project);
+        String keyPivotalToken = "pivotalToken" + getProjectName(project);
+
+        CredentialAttributes credentialAttributes = createCredentialAttributes(keyGithubURL);
+        Credentials credentials = new Credentials(keyGithubURL, this.githubURL);
+        PasswordSafe.getInstance().set(credentialAttributes, credentials);
+
+        credentialAttributes = createCredentialAttributes(keyPivotalURL);
+        credentials = new Credentials(keyPivotalURL, this.pivotalURL);
+        PasswordSafe.getInstance().set(credentialAttributes, credentials);
+
+        credentialAttributes = createCredentialAttributes(keyPivotalToken);
+        credentials = new Credentials(keyPivotalToken, this.token);
+        PasswordSafe.getInstance().set(credentialAttributes, credentials);
+    }
+
+    private String getProjectName(Project project) {
+        String projectName = "";
+
+        VirtualFile projectDir = ProjectUtil.guessProjectDir(project);
+        if (projectDir != null) {
+            projectName = projectDir.getName();
+        }
+        return projectName;
+    }
+
+    private CredentialAttributes createCredentialAttributes(String key) {
+        return new CredentialAttributes(CredentialAttributesKt.generateServiceName("TaitiSystem", key));
     }
 }
