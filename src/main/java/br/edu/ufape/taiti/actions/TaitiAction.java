c943c571b8c7d018748294b18ebcb165c4a28c8e
Gabriel Viana <gabrielalmeidavn@gmail.com>
2021-10-22 11:24:23 -0300

    create the run conflict analysis button

--- src/main/java/br/edu/ufape/taiti/actions/TaitiAction.java
+++ src/main/java/br/edu/ufape/taiti/actions/TaitiAction.java
@@ -1,50 +1,17 @@
 package br.edu.ufape.taiti.actions;
 
-import br.edu.ufape.taiti.exceptions.HttpException;
-import br.edu.ufape.taiti.gui.MainPanel;
 import br.edu.ufape.taiti.gui.TaitiDialog;
-import br.edu.ufape.taiti.service.PivotalTracker;
-import br.edu.ufape.taiti.settings.TaitiSettingsState;
-import br.edu.ufape.taiti.tool.ScenarioTestInformation;
-import br.edu.ufape.taiti.tool.TaitiTool;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class TaitiAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         Project project = event.getProject();
         TaitiDialog taitiDialog = new TaitiDialog(project);
-        TaitiSettingsState settings = TaitiSettingsState.getInstance(project);
-
-        MainPanel mainPanel = taitiDialog.getMainPanel();
-        settings.retrieveStoredCredentials(project);
 
         taitiDialog.show();
-
-        if (taitiDialog.isOK()) {
-            String taskID = mainPanel.getTextTaskID().getText().replace("#", "");
-            ArrayList<ScenarioTestInformation> scenarios = mainPanel.getScenarios();
-
-            TaitiTool taiti = new TaitiTool(settings.getGithubURL(), Integer.parseInt(taskID), scenarios, project);
-            PivotalTracker pivotalTracker = new PivotalTracker(settings.getToken(), settings.getPivotalURL(), taskID);
-
-            try {
-                File file = taiti.createScenariosFile();
-                pivotalTracker.saveScenarios(file);
-                taiti.deleteScenariosFile();
-            } catch (IOException e) {
-                System.out.println("Erro ao criar o arquivo!");
-            } catch (HttpException e) {
-                System.out.println(e.getStatusText() + " - " + e.getStatusNumber());
-                taiti.deleteScenariosFile();
-            }
-        }
     }
 }
--- src/main/java/br/edu/ufape/taiti/gui/TaitiDialog.java
+++ src/main/java/br/edu/ufape/taiti/gui/TaitiDialog.java
@@ -1,13 +1,23 @@
 package br.edu.ufape.taiti.gui;
 
+import br.edu.ufape.taiti.exceptions.HttpException;
+import br.edu.ufape.taiti.service.PivotalTracker;
+import br.edu.ufape.taiti.settings.TaitiSettingsState;
+import br.edu.ufape.taiti.tool.ScenarioTestInformation;
+import br.edu.ufape.taiti.tool.TaitiTool;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.ui.table.JBTable;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jsoup.internal.StringUtil;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class TaitiDialog extends DialogWrapper {
 
@@ -15,20 +25,33 @@ public class TaitiDialog extends DialogWrapper {
     private final JTextField textTaskID;
     private final JBTable table;
 
+    private PivotalTracker pivotalTracker;
+    private TaitiTool taiti;
+
+    private final Project project;
+
     public TaitiDialog(Project project) {
         super(true);
 
-        mainPanel = new MainPanel(project);
-        textTaskID = mainPanel.getTextTaskID();
-        table = mainPanel.getTable();
+        this.mainPanel = new MainPanel(project);
+        this.textTaskID = mainPanel.getTextTaskID();
+        this.table = mainPanel.getTable();
+        this.project = project;
 
         setTitle("TAITI");
         setSize(1000,810);
         init();
     }
 
-    public MainPanel getMainPanel() {
-        return mainPanel;
+    private void prepareServices() {
+        TaitiSettingsState settings = TaitiSettingsState.getInstance(project);
+        settings.retrieveStoredCredentials(project);
+
+        String taskID = textTaskID.getText().replace("#", "");
+        ArrayList<ScenarioTestInformation> scenarios = mainPanel.getScenarios();
+
+        taiti = new TaitiTool(settings.getGithubURL(), Integer.parseInt(taskID), scenarios, project);
+        pivotalTracker = new PivotalTracker(settings.getToken(), settings.getPivotalURL(), project);
     }
 
     @Override
@@ -38,7 +61,7 @@ public class TaitiDialog extends DialogWrapper {
 
     @Override
     public @Nullable JComponent getPreferredFocusedComponent() {
-        return mainPanel.getTextTaskID();
+        return textTaskID;
     }
 
     @Override
@@ -63,5 +86,57 @@ public class TaitiDialog extends DialogWrapper {
         }
 
         return null;
+    }
+
+    @Override
+    protected Action @NotNull [] createActions() {
+        return new Action[]{getOKAction(), new RunConflictAnalysisAction(), getCancelAction()};
+    }
+
+    @Override
+    protected void doOKAction() {
+        super.doOKAction();
+
+        prepareServices();
+
+        String taskID = textTaskID.getText().replace("#", "");
+
+        try {
+            File file = taiti.createScenariosFile();
+            pivotalTracker.saveScenarios(file, taskID);
+            taiti.deleteScenariosFile();
+        } catch (IOException e) {
+            System.out.println("Erro ao criar o arquivo!");
+        } catch (HttpException e) {
+            System.out.println(e.getStatusText() + " - " + e.getStatusNumber());
+            taiti.deleteScenariosFile();
+        }
+    }
+
+    protected class RunConflictAnalysisAction extends DialogWrapperAction {
+        protected RunConflictAnalysisAction() {
+            super("Run Conflict Analysis");
+            putValue(Action.NAME, "Run Conflict Analysis");
+        }
+
+        @Override
+        protected void doAction(ActionEvent e) {
+            if (doValidate() == null) {
+                getOKAction().setEnabled(isOkEnabled());
+                doOKAction();
+
+                try {
+                    ArrayList<File> files = pivotalTracker.downloadFiles();
+                    taiti.createTestI(files);
+
+                } catch (HttpException exception) {
+                    System.out.println(exception.getStatusText() + " - " + exception.getStatusNumber());
+                }
+            }
+        }
+
+        boolean isOkEnabled() {
+            return true;
+        }
     }
 }
